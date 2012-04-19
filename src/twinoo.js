0468b1653ca30fa9b35312fa85ec8fe087b40f85
Acaz <acazsouza@gmail.com>
2012-04-19 00:20:26 -0300

    getSecondNumber function and tests

--- src/twinoo.js
+++ src/twinoo.js
@@ -1,6 +1,6 @@
 //http://www.kongregate.com/games/zigah111/twinoo-the-brain-train
 
-var operators = ['+', '-', '/'];
+var operators = ['+', '-', '*', '/'];
 
 function getRandomOperator(operators) {
     var length = operators.length;
@@ -9,8 +9,26 @@ function getRandomOperator(operators) {
     return randomOperator;
 }
 
-function getRandomNumber() {
-    return Math.floor((Math.random() * 9) + 1);
+function getFirstNumber(operator) {
+    if (operator == '+') {
+        return Math.floor((Math.random() * 8) + 1);
+    } else if (operator == '-') {
+        return Math.floor((Math.random() * 9) + 1);
+    } else if (operator == '*') {
+        return Math.floor((Math.random() * 9) + 1);
+    } else if (operator == '/') {
+        return Math.floor((Math.random() * 9) + 1);
+    }
+
+    return null;
+}
+
+function getSecondNumber(firstNumber, operator) {
+    if (operator == '+') {
+        return Math.floor((Math.random() * (9 - firstNumber)) + 1);
+    }
+
+    return null;
 }
 
 function getExpressionString(firstNumber, operator, secondNumber) {
