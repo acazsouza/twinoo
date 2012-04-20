c657ce55dac56428b7764eb1804e8cadfeb512b7
Acaz <acazsouza@gmail.com>
2012-04-20 00:18:24 -0300

    abstract math.floor(math.random()), implementing getFirstNumber and getSecondNumber

--- src/twinoo.js
+++ src/twinoo.js
@@ -1,23 +1,23 @@
 //http://www.kongregate.com/games/zigah111/twinoo-the-brain-train
 
-var operators = ['+', '-', '*', '/'];
+/*var operators = ['+', '-', '*', '/'];
 
 function getRandomOperator(operators) {
     var length = operators.length;
     var randomOperator = operators[Math.floor(Math.random() * length)];
 
     return randomOperator;
-}
+}*/
 
 function getFirstNumber(operator) {
     if (operator == '+') {
-        return Math.floor((Math.random() * 8) + 1);
+        return getRandomNumber(8);
     } else if (operator == '-') {
-        return Math.floor((Math.random() * 9) + 1);
+        return getRandomNumber(9);
     } else if (operator == '*') {
-        return Math.floor((Math.random() * 9) + 1);
+        return getRandomNumber(9);
     } else if (operator == '/') {
-        return Math.floor((Math.random() * 9) + 1);
+        return getRandomNumber(9);
     }
 
     return null;
@@ -25,16 +25,25 @@ function getFirstNumber(operator) {
 
 function getSecondNumber(firstNumber, operator) {
     if (operator == '+') {
-        return Math.floor((Math.random() * (9 - firstNumber)) + 1);
+        return getRandomNumber(9 - firstNumber);
+    } else if (operator == '-') {
+        return getRandomNumber();
     }
 
     return null;
 }
 
-function getExpressionString(firstNumber, operator, secondNumber) {
+function getRandomNumber(limit) {
+    if (!limit)
+        limit = 9;
+
+    return Math.floor((Math.random() * limit) + 1);
+}
+
+/*function getExpressionString(firstNumber, operator, secondNumber) {
     return (firstNumber.toString() + operator.toString() + secondNumber.toString());
 }
 
 function getAnswer(expression) {
     return eval(expression);
-}
+}*/
