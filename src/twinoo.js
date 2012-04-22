b2e6228e4067ff560d84d69afeaed4a4c3a6c5b4
Acaz <acazsouza@gmail.com>
2012-04-22 00:32:04 -0300

    add except argument in getRandomNumber and a pattern to manage params

--- src/twinoo.js
+++ src/twinoo.js
@@ -1,7 +1,5 @@
 //http://www.kongregate.com/games/zigah111/twinoo-the-brain-train
 
-var operators = ['+', '+', '+', '+', '-', '-', '-', '/', '/', '*'];
-
 function getRandomOperator(operators) {
     var length = operators.length;
     var randomOperator = operators[Math.floor(Math.random() * length)];
@@ -11,13 +9,13 @@ function getRandomOperator(operators) {
 
 function getFirstNumber(operator) {
     if ('+' == operator) {
-        return getRandomNumber(8);
+        return getRandomNumber({ limit: 8 });
     } else if ('-' == operator) {
-        return getRandomNumber(9);
+        return getRandomNumber({ limit: 8, indexDelay: 2 });
     } else if ('*' == operator) {
-        return getRandomNumber(9);
+        return getRandomNumber({ limit: 9 });
     } else if ('/' == operator) {
-        return getRandomNumber(7, 2);
+        return getRandomNumber({ limit: 8, indexDelay: 2 });
     }
 
     return null;
@@ -25,59 +23,107 @@ function getFirstNumber(operator) {
 
 function getSecondNumber(firstNumber, operator) {
     if ('+' == operator) {
-        return getRandomNumber(9 - firstNumber);
+        return getRandomNumber({ limit: 9 - firstNumber });
     } else if ('-' == operator) {
-        if (9 == firstNumber)
-            return getRandomNumber(8);
-        else {
-            var randomNumber = getRandomNumber(9);
-            while (firstNumber == randomNumber) {
-                randomNumber = getRandomNumber(9);
+        return getRandomNumber({ limit: 9 - (10 - firstNumber) });
+    } else if ('*' == operator) {
+        if (2 == firstNumber) {
+            return getRandomNumber({ limit: 4 });
+        } else if (3 == firstNumber) {
+            return getRandomNumber({ limit: 3 });
+        } else if (4 == firstNumber) {
+            return getRandomNumber({ limit: 2 });
+        }
+
+        return 1;
+    } else if ('/' == operator) {
+        if (2 == firstNumber) {
+            return getRandomNumber({ limit: 2 });
+        } else if (3 == firstNumber) {
+            var randomNumber = getRandomNumber({ limit: 3 });
+            while (2 == randomNumber) {
+                randomNumber = getRandomNumber({ limit: 3 });
+            }
+
+            return randomNumber;
+        } else if (4 == firstNumber) {
+            return getRandomNumber({ limit: 2 });
+        } else if (5 == firstNumber) {
+            var randomNumber = getRandomNumber({ limit: 5 });
+            while (2 == randomNumber || 3 == randomNumber || 4 == randomNumber) {
+                randomNumber = getRandomNumber({ limit: 5 });
+            }
+
+            return randomNumber;
+        } else if (6 == firstNumber) {
+            var randomNumber = getRandomNumber({ limit: 6 });
+            while (5 == randomNumber || 4 == randomNumber) {
+                randomNumber = getRandomNumber({ limit: 6 });
+            }
+
+            return randomNumber;
+        } else if (7 == firstNumber) {
+            var randomNumber = getRandomNumber({ limit: 5 });
+            while (2 == randomNumber || 3 == randomNumber || 4 == randomNumber || 5 == randomNumber || 6 == randomNumber) {
+                randomNumber = getRandomNumber({ limit: 5 });
+            }
+
+            return randomNumber;
+        } else if (8 == firstNumber) {
+            var randomNumber = getRandomNumber({ limit: 8 });
+            while (3 == randomNumber || 5 == randomNumber || 6 == randomNumber || 7 == randomNumber) {
+                randomNumber = getRandomNumber({ limit: 8 });
+            }
+
+            return randomNumber;
+        } else if (9 == firstNumber) {
+            var randomNumber = getRandomNumber({ limit: 9 });
+            while (2 == randomNumber || 4 == randomNumber || 5 == randomNumber || 6 == randomNumber || 7 == randomNumber || 8 == randomNumber) {
+                randomNumber = getRandomNumber({ limit: 9 });
             }
 
             return randomNumber;
         }
-    } else if ('*' == operator) {
-        if (2 == firstNumber) {
-            return getRandomNumber(4);
-        } else if (3 == firstNumber) {
-            return getRandomNumber(3);
-        } else if (4 == firstNumber) {
-            return getRandomNumber(2);
-        }
-
-        return 1;
     }
 
     return null;
 }
 
-window.prevRandomNumber = -1;
-function getRandomNumber(limit, indexDelay) {
-    //limit = Number(limit);
-    if (!limit) limit = 9;
-    if (!indexDelay) indexDelay = 1;
+function getRandomNumber(params) {
+    var o = {
+        limit: 9,
+        indexDelay: 1,
+        except: null,
+        prevRandomNumberId: "universalPrevRandomNumberId"
+    }
 
-    if (1 == limit) {
-        window.prevRandomNumber = -1;
+    for (param in params) {
+        o[param] = params[param];
+    }
+
+    if (1 == o.limit) {
+        prevRandomNumber = -1;
         return 1;
     }
 
-    var actualRandomNumber = Math.floor((Math.random() * limit) + indexDelay);
+    var actualRandomNumber = Math.floor((Math.random() * o.limit) + o.indexDelay);
 
-    while (window.prevRandomNumber == actualRandomNumber) {
-        actualRandomNumber = Math.floor((Math.random() * limit) + indexDelay)
+    while (window[o.prevRandomNumberId] == actualRandomNumber || (o.except && (actualRandomNumber == o.except))) {
+        actualRandomNumber = Math.floor((Math.random() * o.limit) + o.indexDelay)
     }
 
-    window.prevRandomNumber = actualRandomNumber;
+    window[o.prevRandomNumberId] = actualRandomNumber;
 
     return actualRandomNumber;
 }
 
-/*function getExpressionString(firstNumber, operator, secondNumber) {
-    return (firstNumber.toString() + operator.toString() + secondNumber.toString());
-}
+function buildExpression(firstNumber, operator, secondNumber) {
+    var answer = Number(eval(firstNumber.toString() + operator.toString() + secondNumber.toString()));
 
-function getAnswer(expression) {
-    return eval(expression);
-}*/
+    return {
+        firstNumber: firstNumber,
+        operator: operator,
+        secondNumber: secondNumber,
+        correctAnswer: answer
+    }
+}
